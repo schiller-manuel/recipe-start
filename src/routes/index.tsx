966f03c7669f206abca45ce0db20e0b73da5c380
Manuel Schiller <manuel.schiller@caligano.de>
2025-10-18 22:20:32 +0200

    start script, add links to recipes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
@@ -7,6 +7,9 @@ function RouteComponent() {
   return (
     <main>
       <h1>Hello world!</h1>
+      <Link to="/recipes/$id" params={{ id: '1' }}>View Chocolate Cookie Recipe</Link>
+      <br />
+      <Link to="/recipes/$id" params={{ id: '2' }}>View Pizza Margeritha Recipe</Link>
     </main>
   )
 }
